64d58faf991f4903c53d627f2dc3e69082899ee1
ten <ten20031017-sty@ezweb.ne.jp>
2024-09-06 00:42:37 +0900

    blogページを作成。

---
+++ components/Post.js
@@ -0,0 +1,14 @@
+function Post({ post }) {
+  return (
+    <div>
+      <span>{post.id}</span>
+      {" : "}
+
+      <span className="cursor-pointer text-blue-500 border-blue-500 hover:bg-gray-200">
+        {post.title}
+      </span>
+    </div>
+  );
+}
+
+export default Post;
